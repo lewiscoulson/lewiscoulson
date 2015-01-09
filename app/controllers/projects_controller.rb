bf892f38c28e73408ca4020dfb9a825abe470310
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-01-09 15:58:53 +0000

    added posts and projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,24 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all.order('created_At DESC')
+  end
+
+  def new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+
+    redirect_to @project
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:name, :date, :skills, :images, :placeholder)
+    end
+end
