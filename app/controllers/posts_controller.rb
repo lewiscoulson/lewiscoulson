54b5c2e1c22b74b1e0c2ced6609835beaae08ba8
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-01-09 16:49:00 +0000

    delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,24 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order('created_At DESC')
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.save
-
-    redirect_to @post
+    @post = Post.new
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -29,11 +33,18 @@ class PostsController < ApplicationController
     else
       render 'edit'
     end
+  end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
