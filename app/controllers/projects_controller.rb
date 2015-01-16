387e7c3ed4964c19017b12447958c481728157fd
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-01-16 14:55:27 +0000

    skeleton complete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,6 +19,27 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Post.find(params[:id])
+
+    if @project.update(params[:project].permit(:name, :date, :skills, :images, :placeholder))
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    redirect_to projects_path
+  end
+
   private
     def project_params
       params.require(:project).permit(:name, :date, :skills, :images, :placeholder)
